4e82619bc41a4faa650ec8a75a502cd2d1db79df
NeoDevlin <neodevlin@gmail.com>
2014-12-10 22:11:32 +0100

    portalapi as a function

--- go-client.go
+++ go-client.go
@@ -10,6 +10,12 @@ import (
 )
 
 func main() {
+
+	portalapi("authenticate")
+
+}
+
+func portalapi(action string) {
 	Transport := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
@@ -17,13 +23,14 @@ func main() {
 	urlValues := url.Values{}
 	urlValues.Set("login", "ilg2.4.59")
 	urlValues.Set("password", "FWSKXFym")
-	urlValues.Set("action", "authenticate")
+	//urlValues.Set("action", "authenticate")
+	urlValues.Set("action", action)
+	urlValues.Set("password_digest", "f76bbb94d649d9d5958882501eda86bac57842c3")
 	response, err := client.PostForm("https://controller.mobile.lan/portal_api.php", urlValues)
 	if err != nil {
 		log.Print(err)
 	} else {
 		defer response.Body.Close()
-		//log.Print(response.Header)
 		ioresp, _ := ioutil.ReadAll(response.Body)
 		log.Print(string(ioresp))
 	}
