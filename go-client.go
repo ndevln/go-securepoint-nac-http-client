9c0f12557f285e702fab667940a73e603dfee833
NeoDevlin <neodevlin@gmail.com>
2014-12-13 15:49:28 +0100

    Logging to File and stdout

--- go-client.go
+++ go-client.go
@@ -3,17 +3,27 @@ package main
 
 import (
 	"crypto/tls"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 )
 
 func main() {
+	// Setup Logfile
+	logfile, err := os.OpenFile("log.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Print("Failed to open log file", ":", err)
+	}
+	multiLogger := io.MultiWriter(logfile, os.Stdout)
+	log.SetOutput(multiLogger)
+	// Login Data
 	login := "ilg2.4.59"
 	password := "FWSKXFym"
 	passworddigest := "f76bbb94d649d9d5958882501eda86bac57842c3"
-
+	//Calling the Portal Api
 	log.Print("Disconnect started...")
 	log.Print(portalapi(login, password, passworddigest, "disconnect"))
 	log.Print("Connect started...")
